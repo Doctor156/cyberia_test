5974b57c2046322fdc05b0d45d4ff30567ed5658
Doctor_156 <sumatoxin.m@yandex.ru>
2022-10-23 21:00:46 +0700

    * small refuck of fucktories

--- example-app/database/factories/AuthorFactory.php
+++ example-app/database/factories/AuthorFactory.php
@@ -4,15 +4,9 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class AuthorFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition(): array
     {
         $users = User::all()->pluck('id')->toArray();
--- example-app/database/factories/BookFactory.php
+++ example-app/database/factories/BookFactory.php
@@ -4,15 +4,9 @@ namespace Database\Factories;
 
 use App\Models\Author;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class BookFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition(): array
     {
         $freeAuthorId = Author::with('books')->doesntHave('books')->inRandomOrder()->first()->id;
